f4139634f676fbe731d45a3c360e00203aa18297
WanderleyJunior <wanderleyjunr@gmail.com>
2021-09-14 11:15:39 -0300

    adding storybook and its settings

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles'
 
-const Main: React.FC = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, ReactJS, NextJS e Styled Component'
+}) => (
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
       alt="Image de um átomo e React Avançado escrito ao lado."
     />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>
-      Typescript, ReactJS, NextJS e Styled Component
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Ilustration
       src="/img/hero-illustration.svg"
       alt="desenvolvedor de frente para tela com codigo"
