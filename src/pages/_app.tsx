7a33856065139ba9fe984068d5d6cf6a1cf15333
WanderleyJunior <wanderleyjunr@gmail.com>
2021-09-14 00:41:31 -0300

    create of GlbalStyles and page _app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Além do Nível - Boilerplate</title>
+        <meta
+          name="description"
+          content="A simple project starter to work with Typescript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
