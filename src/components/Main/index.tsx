ec2848fd68d6499a580b5715c92c25f66e8e4a45
WanderleyJunior <wanderleyjunr@gmail.com>
2021-09-14 00:59:15 -0300

    create styles of components Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main: React.FC = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Image de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS e Styled Component
+    </S.Description>
+    <S.Ilustration
+      src="/img/hero-illustration.svg"
+      alt="desenvolvedor de frente para tela com codigo"
+    />
+  </S.Wrapper>
 )
 
 export default Main
